82211f33bf1c0f9238b2c94c758b9fec72325b58
Nadia <primisen17@gmail.com>
2021-03-09 20:41:48 +0300

    Add findPlaceByName(String name) to PlaceRepository

--- src/main/java/com/nadia/telegram_bot/fine_place_bot/repository/PlaceRepository.java
+++ src/main/java/com/nadia/telegram_bot/fine_place_bot/repository/PlaceRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Repository
 public interface PlaceRepository extends CrudRepository<Place, Integer> {
 
+    Place findPlaceByName(String name);
+
     @Query(value = "select place_name from place where place.city = ?1", nativeQuery = true)
     List<String> getPlacesNamesByCity(String city);
 }
